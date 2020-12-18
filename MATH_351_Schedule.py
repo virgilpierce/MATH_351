da9df5946a3b92d00aa8d6d539072f5e3cb240d9
Virgil Pierce <virgilpierce@gmail.com>
2020-12-18 15:25:31 -0700

    Updates to the schedule

--- MATH_351_Schedule.py
+++ MATH_351_Schedule.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[14]:
 
 
 import pandas as pa
@@ -9,69 +9,70 @@ import datetime as dt
 pa.set_option('max_colwidth', 200)
 
 
-# In[2]:
+# In[15]:
 
 
-first_day = dt.date(2020, 1, 11)
+first_day = dt.date(2021, 1, 11)
 
 
-# In[3]:
+# In[16]:
 
 
-holidays = [dt.date(2020, 1, 18) ]
-spring_break_start = dt.date(2020, 3, 13)
-spring_break_end = dt.date(2020, 3, 21)
+holidays = [dt.date(2021, 1, 18) ]
+spring_break_start = dt.date(2021, 3, 13)
+spring_break_end = dt.date(2021, 3, 21)
 spring_break_delta = spring_break_end - spring_break_start
 holidays += [ spring_break_start + dt.timedelta(i) for i in range(spring_break_delta.days+1)]
 
 
-# In[4]:
+# In[17]:
 
 
-last_day = dt.date(2020, 4, 30)
+last_day = dt.date(2021, 4, 30)
 
 
-# In[5]:
+# In[18]:
 
 
-final_exam = dt.date(2020, 5, 7)
+final_exam = dt.date(2021, 5, 7)
 
 
-# In[6]:
+# In[19]:
 
 
 semester_length = last_day - first_day
 semester_length
 
 
-# In[18]:
+# In[20]:
 
 
 class_days = []
 for i in range((semester_length.days //7 + 1)):
-    class_days += [first_day + dt.timedelta(1) + dt.timedelta(i*7), first_day + dt.timedelta(1) + dt.timedelta(i*7+2)]
+    class_days += [first_day + dt.timedelta(i*7+1), first_day + dt.timedelta(i*7+3)]
     # shift first day by 1 for TR class
 class_days += [final_exam]
 
 
-# In[19]:
+# In[21]:
 
 
 schedule = pa.DataFrame(class_days, columns = ['Day'])
-weekday_dict = {5:'Monday', 6:'Tuesday', 0:'Wednesday', 1:'Thursday', 2:'Friday', 3:'Saturday', 4:'Sunday'}
+weekday_dict = {0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 
+                5:'Saturday', 6:'Sunday'}
 for i in range(schedule.shape[0]):
     schedule.loc[i,'Week_Day'] = schedule.loc[i, 'Day'].weekday()
 schedule.Week_Day = schedule.Week_Day.map(weekday_dict)
 
 
-# In[21]:
+# In[22]:
 
 
 for holiday in holidays:
     schedule.loc[schedule.Day==holiday, 'Title'] = 'No Class - University Holiday'
 
 
-# In[22]:
+# In[23]:
 
 
 def add_day(title='', description='', notes=''):
@@ -92,7 +93,7 @@ def add_day(title='', description='', notes=''):
     
 
 
-# In[33]:
+# In[25]:
 
 
 day = 0
@@ -101,8 +102,6 @@ day = 0
 
 add_day('Discrete Random Variables', 
         'Discrete random variables and their probability distributions, simmulating discrete random variables')
-
-add_day('Expected Value of Discrete Random Variable', 'Computing the expected value of a discrete random variable')
 
 add_day('Binomial and Geometric Probability Distributions')
 
@@ -159,8 +158,6 @@ add_day('Point Estimators')
 
 add_day('Confidence Intervals')
 
-add_day('Sample Size Concerns')
-
 add_day('Methods of Estimation')
 
 # Hypothesis Testing
@@ -172,6 +169,12 @@ add_day('Type II Error Probabilities')
 add_day('Hypothesis Testing and Confidence Intervals')
 
 add_day('More Hypothesis Testing')
+
+# Bayesian Methods
+
+add_day('Bayesian Priors, Posteriors, and Estimators')
+
+add_day('Bayesian Tests of Hypothesis and Baysian Credible Intervals')
 
 add_day('Considerations in Experimental Designs')
 
